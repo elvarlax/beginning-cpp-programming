a68ff5bd801ed24f7d47a22ed6657fd8c39dfb6c
Elvar Laxdal <elvarlax@gmail.com>
2019-12-29 19:50:52 +0000

    Coding Exercises 1-3

---
+++ Section 05/Coding Exercise 1/Coding Exercise 1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Coding Exercise #1 - Using cout and the insertion operator to say Hi to Frank
+ * Write a single output statement using cout that displays "Hi Frank!" to the console.
+ */
+#include <iostream>
+
+using namespace std;
+
+void say_hi() {
+	cout << "Hi Frank!";
+}
+
+int main() {
+	say_hi();
+	return 0;
+}
---
+++ Section 05/Coding Exercise 3/Coding Exercise 3.cpp
@@ -0,0 +1,24 @@
+/*
+ * Coding Exercise #3 - Using cin and the extraction operator
+ * In this exercise you will write code using cin
+ * and the extraction operator >> to allow a user to enter their date of birth.
+ *
+ * The variable m represents the month, d represents the day, y represents the year.
+ * Assume that the user will enter their date of birth in the order of
+ * month, day, year, with each value being separated by a white space.
+ */
+#include <iostream>
+using namespace std;
+
+void date_of_birth() {
+	int m{};
+	int d{};
+	int y{};
+	cin >> m >> d >> y;
+	cout << m << " " << d << " " << y;
+}
+
+int main() {
+	date_of_birth();
+	return 0;
+}
